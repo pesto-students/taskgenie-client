ac557f5474039845e9d77e3d5b5ec1512154b84b
Ravi <rathoreravi1508@gmail.com>
2024-02-09 00:03:02 +0530

    set theme for button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,51 @@
+import { Button, Container, Stack, TextField, InputAdornment, InputLabel, Autocomplete } from '@mui/material'
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import React from 'react'
 
 const Home = () => {
+  const options = [
+    { label: "delhi", id: 1 },
+    { label: "mumbai", id: 2 },
+    { label: "chennai", id: 3 },
+    { label: "kolkata", id: 4 },
+    { label: "dehradun", id: 5 },
+  ]
   return (
-    <div>Home</div>
+    <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+      <Stack rowGap={'20px'} sx={{ padding: '10px 50px', width: '400px', textAlign: 'center' }}>
+
+        <div sx={{ width: '100%' }}>
+          {/* Email */}
+          <InputLabel sx={{ textAlign: 'left' }}>Email</InputLabel>
+          <TextField placeholder='Enter email' InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <EmailOutlinedIcon />
+              </InputAdornment>
+            )
+          }}
+            sx={{ width: '100%' }}
+          />
+          {/* Password */}
+          <InputLabel sx={{ textAlign: 'left', mt: '20px' }}>Password</InputLabel>
+          <TextField placeholder='Enter password' InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <LockOutlinedIcon />
+              </InputAdornment>
+            )
+          }}
+            sx={{ width: '100%' }}
+          />
+          {/* Autocomplete */}
+          <Autocomplete sx={{ mt: '20px' }} options={options} renderInput={(params) => <TextField placeholder='eg. Delhi' {...params}  />} />
+        </div>
+        <div>
+          <Button variant='contained' sx={{ width: '100%' }}>Sign Up</Button>
+        </div>
+      </Stack>
+    </Container>
   )
 }
 
--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,46 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
-  typography: {},
-  components: {},
+  palette: {
+    primary: {
+      main: '#8659d3',
+      light: '#9e7adb',
+      dark: '#5d3e93'
+    },
+  },
+
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 12,
+          lineHeight: 3,
+          textTransform: 'none',
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          borderRadius: 12,
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          borderRadius: 12,
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: '#000',
+          fontWeight: 'bold'
+        }
+      }
+    }
+  },
 });
 export default theme;
