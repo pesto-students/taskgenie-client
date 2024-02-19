d62471528d13ae31d85737b414ca9eee1016f591
Saksham <sakshamtewari3@gmail.com>
2024-02-19 08:21:32 +0530

    Chip, Dialog Components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Button from '../components/Button'
+import Chip from '../components/Chip'
+import Dialog from '../components/Dialog'
 
 const Home = () => {
   return (
     <>
+    <Chip>Open</Chip>
+    <Dialog>Hello</Dialog>
     </>
   )
 }
