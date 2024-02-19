079df697293125ff5fb1551ad9ef49834bfddd74
Ravi <rathoreravi1508@gmail.com>
2024-02-19 17:02:36 +0530

    create card comoponent

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { Container, Box } from '@mui/material/';
+import Card from '../components/Card/Card';
 const SignUp = () => {
     return (
-        <div>SignUp</div>
+        <>
+            <Container>
+                <Box>
+                    {/* Header */}
+                    <h3>TaskGenie</h3>
+                </Box>
+                <Box>
+                    <Card>
+                        This is card
+                    </Card>
+                </Box>
+            </Container>
+        </>
     )
 }
 
