3ed4274f177c897752366b30cc4f752fef975cdb
Ravi <rathoreravi1508@gmail.com>
2024-02-25 19:16:52 +0530

    format PostTask.jsx

--- src/pages/PostTask.jsx
+++ src/pages/PostTask.jsx
@@ -15,10 +15,10 @@ import ToggleButton from "../components/ToggleButton";
 import SearchTextField from "../components/SearchTextField";
 import { Stepper } from "../components/UI/Stepper";
 import TextArea from "../components/UI/TextArea";
-import { InputAdornment } from '@mui/material/';
-import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
-import CurrencyRupeeOutlinedIcon from '@mui/icons-material/CurrencyRupeeOutlined';
-import AddAPhotoOutlinedIcon from '@mui/icons-material/AddAPhotoOutlined';
+import { InputAdornment } from "@mui/material/";
+import FmdGoodOutlinedIcon from "@mui/icons-material/FmdGoodOutlined";
+import CurrencyRupeeOutlinedIcon from "@mui/icons-material/CurrencyRupeeOutlined";
+import AddAPhotoOutlinedIcon from "@mui/icons-material/AddAPhotoOutlined";
 import DatePicker from "../components/DatePicker";
 
 const PostTask = () => {
@@ -37,176 +37,197 @@ const PostTask = () => {
 
   const handleNext = () => {
     setCurrentStep(1);
-  }
+  };
 
   const handleBack = () => {
     setCurrentStep(0);
-  }
+  };
 
-  const handleSubmit = () =>{
+  const handleSubmit = () => {
     console.log("submitted");
-  }
+  };
   return (
     <>
-    {currentStep === 0 ?
-      <Box sx={{ padding: "0 1.2rem" }}>
-        {/* Stepper */}
-        <Box sx={{ margin: "2rem 0" }}>
-          <Stepper
-            steps={2}
-            currentStep={currentStep}
-          />
-        </Box>
-        {/* Main */}
-        <Card sx={{ padding: "1rem" }}>
-          <Box sx={{ mt: "1rem" }}>
-            <FormContainer defaultValues={{ title: "" }}>
-              <Stack
-                gap={"1rem"}
-                alignItems={"center"}
-              >
-                {/* Title */}
-
-                <FormControl>
-                  <InputLabel>What needs to be done?</InputLabel>
-                  <TextField
-                    name='title'
-                    required
-                    type='text'
-                    placeholder='Enter the title of task here'
-                    sx={{ width: "100%" }}
-                  />
-                </FormControl>
-
-                <FormControl sx={{ marginTop: "2rem" }}>
-                  <InputLabel>Where is it located?</InputLabel>
-                  <ToggleButtonGroup
-                    value={location}
-                    onChange={handleLocationChange}
-                    sx={{ justifyContent: "center" }}
-                  >
-                    <ToggleButton value='in-person'>In Person</ToggleButton>
-                    <ToggleButton value='remote'>Remote</ToggleButton>
-                  </ToggleButtonGroup>
-                  {/* <SearchTextField>Search Location</SearchTextField> */}
-                </FormControl>
-
-                <FormControl sx={{ marginTop: "2rem" }}>
-                  <InputLabel>When do you need this done?</InputLabel>
-                  <ToggleButtonGroup
-                    value={time}
-                    onChange={handleTimeChange}
-                    sx={{ justifyContent: "center" }}
-                  >
-                    <ToggleButton value='on'>On</ToggleButton>
-                    <ToggleButton value='before'>Before</ToggleButton>
-                    <ToggleButton value='flexible'>Flexible</ToggleButton>
-                  </ToggleButtonGroup>
-                </FormControl>
-
-
-
-                {/* Date Pick */}
-              {(time === 'on' || time === 'before')?
-                <FormControl sx={{alignItems:'center', display:'flex',justifyContent:'center'}}>
-                  <DatePicker/>
-                </FormControl>
-                : null
-              }
-
-                {/* Submit */}
-                <FormControl sx={{ marginTop: "2rem" }}>
-                  <Button
-                    variant='contained'  
-                    onClick={handleNext}
-                    sx={{ width: "100%" }}
-                  >
-                    Next
-                  </Button>
-                </FormControl>
-              </Stack>
-            </FormContainer>
+      {currentStep === 0 ? (
+        // First Step
+        <Box sx={{ padding: "0 1.2rem" }}>
+          {/* Stepper */}
+          <Box sx={{ margin: "2rem 0" }}>
+            <Stepper
+              steps={2}
+              currentStep={currentStep}
+            />
           </Box>
-        </Card>
-      </Box>
+          {/* Main */}
+          <Card sx={{ padding: "1rem" }}>
+            <Box sx={{ mt: "1rem" }}>
+              <FormContainer defaultValues={{ title: "" }}>
+                <Stack
+                  gap={"1rem"}
+                  alignItems={"center"}
+                >
+                  {/* Title */}
 
-      
-      :
+                  <FormControl>
+                    <InputLabel>What needs to be done?</InputLabel>
+                    <TextField
+                      name='title'
+                      required
+                      type='text'
+                      placeholder='Enter the title of task here'
+                      sx={{ width: "100%" }}
+                    />
+                  </FormControl>
 
-      // Next page
+                  <FormControl sx={{ marginTop: "2rem" }}>
+                    <InputLabel>Where is it located?</InputLabel>
+                    <ToggleButtonGroup
+                      value={location}
+                      onChange={handleLocationChange}
+                      sx={{ justifyContent: "center" }}
+                    >
+                      <ToggleButton value='in-person'>In Person</ToggleButton>
+                      <ToggleButton value='remote'>Remote</ToggleButton>
+                    </ToggleButtonGroup>
+                    {/* <SearchTextField>Search Location</SearchTextField> */}
+                  </FormControl>
 
-      <Box sx={{ padding: "0 1.2rem" }}>
-        {/* Stepper */}
-        <Box sx={{ margin: "2rem 0" }}>
-          <Stepper
-            steps={2}
-            currentStep={currentStep}
-          />
+                  <FormControl sx={{ marginTop: "2rem" }}>
+                    <InputLabel>When do you need this done?</InputLabel>
+                    <ToggleButtonGroup
+                      value={time}
+                      onChange={handleTimeChange}
+                      sx={{ justifyContent: "center" }}
+                    >
+                      <ToggleButton value='on'>On</ToggleButton>
+                      <ToggleButton value='before'>Before</ToggleButton>
+                      <ToggleButton value='flexible'>Flexible</ToggleButton>
+                    </ToggleButtonGroup>
+                  </FormControl>
+
+                  {/* Date Pick */}
+                  {time === "on" || time === "before" ? (
+                    <FormControl
+                      sx={{
+                        alignItems: "center",
+                        display: "flex",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <DatePicker />
+                    </FormControl>
+                  ) : null}
+
+                  {/* Submit */}
+                  <FormControl sx={{ marginTop: "2rem" }}>
+                    <Button
+                      variant='contained'
+                      onClick={handleNext}
+                      sx={{ width: "100%" }}
+                    >
+                      Next
+                    </Button>
+                  </FormControl>
+                </Stack>
+              </FormContainer>
+            </Box>
+          </Card>
         </Box>
-        {/* Main */}
-        <Card sx={{ padding: "1rem" }}>
-          <Box sx={{ mt: "1rem" }}>
-            <FormContainer onSuccess={handleSubmit} defaultValues={{ description: "" }}>
-              <Stack
-                gap={"1rem"}
-                alignItems={"center"}
-              >
-                {/* Description */}
-
-                <FormControl>
-                  <InputLabel>Task Details</InputLabel>
-                  <TextArea aria-label='Task Details' minRows={7} maxRows={7} />
-                </FormControl>
-
-                <FormControl sx={{ marginTop: "2rem" }}>
-                  <InputLabel>Budget</InputLabel>
-                  <TextField name="budget" type="number" InputProps={{
-                                            startAdornment: (
-                                                <InputAdornment position='start'>
-                                                    <CurrencyRupeeOutlinedIcon sx={{color : 'rgb(0,0,0)'}} />
-                                                </InputAdornment>
-                                            )
-                                        }}/>
-                </FormControl>
-
-                {/* Image upload */}
-
-                <FormControl sx={{marginTop: "2rem" }}>
-                  
-                  <Button variant="outlined" component="label" sx={{height:'5rem', borderColor:'black', color:'black'}} >
-                    Add Photo
-                    <input accept="image/*" hidden type="file" />
-                    <AddAPhotoOutlinedIcon sx={{color:'black'}}/>
-                  </Button>
-                  
-                </FormControl>
-
-                
-
-                {/* Submit */}
-                <FormControl sx={{ marginTop: "2rem" , display: 'flex', gap:'1rem'} }>
-                  
-                  <Button
-                    variant='outlined'
-                    sx={{ width: "100%" }}
-                    onClick={handleBack}
-                  >
-                    Back
-                  </Button>
-                  <Button
-                    variant='contained'
-                    sx={{ width: "100%" }}
-                  >
-                    Get Quotes
-                  </Button>
-                </FormControl>
-              </Stack>
-            </FormContainer>
+      ) : (
+        // Second Step
+        <Box sx={{ padding: "0 1.2rem" }}>
+          {/* Stepper */}
+          <Box sx={{ margin: "2rem 0" }}>
+            <Stepper
+              steps={2}
+              currentStep={currentStep}
+            />
           </Box>
-        </Card>
-      </Box>
+          {/* Main */}
+          <Card sx={{ padding: "1rem" }}>
+            <Box sx={{ mt: "1rem" }}>
+              <FormContainer
+                onSuccess={handleSubmit}
+                defaultValues={{ description: "" }}
+              >
+                <Stack
+                  gap={"1rem"}
+                  alignItems={"center"}
+                >
+                  {/* Description */}
 
-    }
+                  <FormControl>
+                    <InputLabel>Task Details</InputLabel>
+                    <TextArea
+                      aria-label='Task Details'
+                      minRows={7}
+                      maxRows={7}
+                    />
+                  </FormControl>
+
+                  <FormControl sx={{ marginTop: "2rem" }}>
+                    <InputLabel>Budget</InputLabel>
+                    <TextField
+                      name='budget'
+                      type='number'
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position='start'>
+                            <CurrencyRupeeOutlinedIcon
+                              sx={{ color: "rgb(0,0,0)" }}
+                            />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  </FormControl>
+
+                  {/* Image upload */}
+
+                  <FormControl sx={{ marginTop: "2rem" }}>
+                    <Button
+                      variant='outlined'
+                      component='label'
+                      sx={{
+                        height: "5rem",
+                        borderColor: "black",
+                        color: "black",
+                      }}
+                    >
+                      Add Photo
+                      <input
+                        accept='image/*'
+                        hidden
+                        type='file'
+                      />
+                      <AddAPhotoOutlinedIcon sx={{ color: "black" }} />
+                    </Button>
+                  </FormControl>
+
+                  {/* Submit */}
+                  <FormControl
+                    sx={{ marginTop: "2rem", display: "flex", gap: "1rem" }}
+                  >
+                    <Button
+                      variant='outlined'
+                      sx={{ width: "100%" }}
+                      onClick={handleBack}
+                    >
+                      Back
+                    </Button>
+                    <Button
+                      variant='contained'
+                      sx={{ width: "100%" }}
+                    >
+                      Get Quotes
+                    </Button>
+                  </FormControl>
+                </Stack>
+              </FormContainer>
+            </Box>
+          </Card>
+        </Box>
+      )}
     </>
   );
 };
