bb94666d8d10978d5f78a10ce45955b9bf785e09
Saksham Tewari <sakshamtewari3@gmail.com>
2024-03-09 23:21:48 +0530

    Removed error

--- src/components/atoms/Tabs/Tabs.jsx
+++ src/components/atoms/Tabs/Tabs.jsx
@@ -11,21 +11,7 @@ const StyledTabs = styled(MuiTabs)(({ theme }) => {
     borderBottom: `1px solid ${theme.palette.divider}`,
   };
 });
-// const StyledTabs = styled(MuiTabs)(({ theme }) => {
-//     return {
-//         borderBottom: `1px solid ${theme.palette.divider}`,
-//     }
-// })
-
-// const Tabs = ({ children, ...props }) => {
-//     return <StyledTabs {...props}>{children}</StyledTabs>
-// }
-
-
-
-const StyledTabs = styled(MuiTabs)(() => {
-  return {};
-});
+;
 const Tabs = ({ options, defaultValue, onChange, ...props }) => {
   const [selectedValue, setSelectedValue] = useState(defaultValue);
   const handleChange = (_, newValue) => {
