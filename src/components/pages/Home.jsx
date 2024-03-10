75751446c7c8d7bd4559afc349b8d0a23f02b8d1
Saksham Tewari <sakshamtewari3@gmail.com>
2024-03-10 16:17:15 +0530

    Added svg files, modified them as per theme.main color

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -14,6 +14,7 @@ import HouseCleaning from 'assets/cleaning-service.svg?react';
 import Shopping from 'assets/Eco-shopping.svg?react';
 import OpenSource from 'assets/Open-source.svg?react';
 import Electrician from 'assets/Electrician.svg?react';
+import TaskGenie from 'assets/Taskgenie.svg?react';
 
 const sectionPadding = "1.5rem 1rem";
 
@@ -168,8 +169,9 @@ const Home = () => {
         </Typography>
         <Typography variant='body1'>Expand your business and clientele with ease</Typography>
       </Stack>
+      <TaskGenie/> 
       </Box>
-      <OpenSource/>
+      
     </>
   );
 };
