b5959a2136ce0d07597114ee42f579fa641b7bc9
Saksham Tewari <sakshamtewari3@gmail.com>
2024-03-10 21:05:26 +0530

    Added Testimonials, Footer sections

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,6 +11,9 @@ import {
 } from "components/atoms";
 import { useNavigate } from "react-router-dom";
 import DoneIcon from "@mui/icons-material/Done";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import HouseCleaning from "assets/cleaning-service.svg?react";
 import Shopping from "assets/Eco-shopping.svg?react";
 import OpenSource from "assets/Open-source.svg?react";
@@ -136,9 +139,9 @@ const Home = () => {
       >
         <Typography variant='h6'>Find genie in 3 easy Steps</Typography>
         <Stack direction='column'>
-          <Typography variant='body1'>Step 1</Typography>
-          <Typography variant='body1'>Step 2</Typography>
-          <Typography variant='body1'>Step 3</Typography>
+          <Typography variant='subtitle'>Step 1</Typography>
+          <Typography variant='subtitle'>Step 2</Typography>
+          <Typography variant='subtitle'>Step 3</Typography>
         </Stack>
       </Box>
 
@@ -241,18 +244,48 @@ const Home = () => {
         }}
       >
         <Box>
-        <Typography variant='h5'>Testimonials</Typography>
+          <Typography variant='h5'>Testimonials</Typography>
         </Box>
         <Box>
-        <Stack
-          sx={{ marginTop: "1rem" }}
-          direction='column'
-          gap={1}
-        >
-        <HomeCarousel />
-        </Stack>
+          <Stack
+            sx={{ marginTop: "1rem" }}
+            direction='column'
+            gap={1}
+          >
+            <HomeCarousel />
+          </Stack>
         </Box>
-        
+      </Box>
+
+      {/* Footer */}
+      <Box
+        component='footer'
+        className='footer'
+        sx={{
+          backgroundColor: theme.palette.grey[200],
+          padding: sectionPadding,
+        }}
+      >
+        <Box>
+          <Stack direction='column'>
+            <Typography variant='subtitle'>Terms and Conditions</Typography>
+            <Typography variant='subtitle'>FAQ</Typography>
+            <Typography variant='subtitle'>Community Guidelines</Typography>
+            <Typography variant='subtitle'>Contact Us</Typography>
+          </Stack>
+        </Box>
+        <Box sx={{marginTop:'1rem', display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
+          <Typography variant='h5'>
+            TaskGenie
+            </Typography>
+        </Box>
+        <Box sx={{display: 'flex', justifyContent:'space-around', alignItems:'center'}}>
+        <Stack  sx={{ marginTop: "1rem" }} direction='row'>
+          <GitHubIcon sx={{ marginRight: '0.5rem' }}/>
+          <InstagramIcon sx={{ marginRight: '0.5rem' }}/>
+          <TwitterIcon/>
+          </Stack>
+        </Box>
       </Box>
     </>
   );
