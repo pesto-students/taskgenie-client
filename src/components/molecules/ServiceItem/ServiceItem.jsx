a94d8245ea8fbeba7f03af3f00b286f5a89ab434
Ravi Rathore <rathoreravi1508@gmail.com>
2024-06-18 11:26:09 +0530

    add prop validation

--- src/components/molecules/ServiceItem/ServiceItem.jsx
+++ src/components/molecules/ServiceItem/ServiceItem.jsx
@@ -1,27 +1,32 @@
+import { PropTypes } from "prop-types";
 import { Box, Typography } from "components/atoms";
 import { Paper } from "@mui/material";
 import { styled } from "@mui/material/styles";
 const StyledPaper = styled(Paper)(({ theme }) => ({
-  padding: "1rem",
-  textAlign: "center",
-  [theme.breakpoints.up("lg")]: {
-    background: "color",
-  },
-  "& svg": {},
+	padding: "1rem",
+	textAlign: "center",
+	[theme.breakpoints.up("lg")]: {
+		background: "color",
+	},
+	"& svg": {},
 }));
-const ServiceItem = ({ label, image, children, ...props }) => {
-  return (
-    <StyledPaper {...props}>
-      <Typography variant='body2'>{label}</Typography>
-      <Box
-        sx={{
-          overflow: "hidden",
-          margin: "0 auto",
-        }}
-      >
-        {image}
-      </Box>
-    </StyledPaper>
-  );
+const ServiceItem = ({ label, image, ...props }) => {
+	return (
+		<StyledPaper {...props}>
+			<Typography variant='body2'>{label}</Typography>
+			<Box
+				sx={{
+					overflow: "hidden",
+					margin: "0 auto",
+				}}
+			>
+				{image}
+			</Box>
+		</StyledPaper>
+	);
+};
+ServiceItem.propTypes = {
+	label: PropTypes.string.isRequired,
+	image: PropTypes.element,
 };
 export default ServiceItem;
