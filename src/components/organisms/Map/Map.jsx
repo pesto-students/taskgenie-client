51b7afe0c14d77173c5b31aae01787d3fe7ea531
Ravi Rathore <rathoreravi1508@gmail.com>
2024-06-30 15:43:14 +0530

    update tilelayer

--- src/components/organisms/Map/Map.jsx
+++ src/components/organisms/Map/Map.jsx
@@ -3,61 +3,64 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import MapPopup from "components/molecules/MapPopup/MapPopup";
 import RoomIcon from "@mui/icons-material/Room";
 const SetViewOnUpdate = ({ center }) => {
-  const map = useMap();
-  useEffect(() => {
-    const newCenter = [center.lat, center.lng];
-    map.setView(newCenter, 11);
+	const map = useMap();
+	useEffect(() => {
+		const newCenter = [center.lat, center.lng];
+		map.setView(newCenter, 11);
 
-    return () => {};
-  }, [center]);
+		return () => {};
+	}, [center]);
 };
 
 const Map = ({ tasks = [], center, searchRadius = 100, width }) => {
-  const calculateZoomLevel = (radius) => {
-    // Approximate conversion factor from kilometers to meters
-    const metersPerKilometer = 1000;
-    const radiusInMeters = radius * metersPerKilometer;
+	const calculateZoomLevel = (radius) => {
+		// Approximate conversion factor from kilometers to meters
+		const metersPerKilometer = 1000;
+		const radiusInMeters = radius * metersPerKilometer;
 
-    // Approximate conversion factor from meters to degrees
-    const metersPerDegree = 111000;
-    const radiusInDegrees = radiusInMeters / metersPerDegree;
+		// Approximate conversion factor from meters to degrees
+		const metersPerDegree = 111000;
+		const radiusInDegrees = radiusInMeters / metersPerDegree;
 
-    // Use a formula to determine the zoom level based on the radius
-    const zoomLevel = Math.ceil(14 - Math.log(radiusInDegrees) / Math.log(2));
+		// Use a formula to determine the zoom level based on the radius
+		const zoomLevel = Math.ceil(14 - Math.log(radiusInDegrees) / Math.log(2));
 
-    return zoomLevel;
-  };
-
-  const zoomLevel = calculateZoomLevel(searchRadius);
-  return (
-    <>
-      <MapContainer
-        center={[center.lat, center.lng]}
-        zoom={zoomLevel}
-        scrollWheelZoom={false}
-        style={{ width: { width }, height: "95vh" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://www.justicemap.org/terms.php">Justice Map</a> contributors'
-          url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
-        />
-        <SetViewOnUpdate center={center} />
-        {tasks.map((task) => {
-          if (task.location) {
-            const lat = task.location.coordinates[1];
-            const lng = task.location.coordinates[0];
-            return (
-              <>
-                <Marker position={[lat, lng]}>
-                  <MapPopup task={task} />
-                </Marker>
-              </>
-            );
-          }
-        })}
-      </MapContainer>
-    </>
-  );
+		return zoomLevel;
+	};
+	const tileLayerURL =
+		"https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}";
+	const tileLayerAttribution =
+		"Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ";
+	const zoomLevel = calculateZoomLevel(searchRadius);
+	return (
+		<>
+			<MapContainer
+				center={[center.lat, center.lng]}
+				zoom={zoomLevel}
+				scrollWheelZoom={false}
+				style={{ width: { width }, height: "95vh" }}
+			>
+				<TileLayer
+					attribution={tileLayerAttribution}
+					url={tileLayerURL}
+				/>
+				<SetViewOnUpdate center={center} />
+				{tasks.map((task) => {
+					if (task.location) {
+						const lat = task.location.coordinates[1];
+						const lng = task.location.coordinates[0];
+						return (
+							<>
+								<Marker position={[lat, lng]}>
+									<MapPopup task={task} />
+								</Marker>
+							</>
+						);
+					}
+				})}
+			</MapContainer>
+		</>
+	);
 };
 
 export default Map;
