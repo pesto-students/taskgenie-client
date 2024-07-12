6c6a2fef9ae16466ced049c22bb6aca41c50e196
Ravi Rathore <rathoreravi1508@gmail.com>
2024-07-12 18:03:17 +0530

    change format of taskitem in tasklist

--- src/components/molecules/TaskItem/TaskItem.jsx
+++ src/components/molecules/TaskItem/TaskItem.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 // External Imports
 import { Divider } from "@mui/material";
-import Avatar from "@mui/material/Avatar";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
 import { useTheme } from "@mui/material/styles";
 // Internal Imports
@@ -51,36 +51,20 @@ const TaskItem = ({ task }) => {
 							{task.status && <TaskStatusChip status={task.status} />}
 						</Box>
 					</Stack>
-					<Box sx={{ paddingTop: "0.7rem" }}>
-						{/* Task Location */}
 
-						<Stack
-							direction='row'
-							alignItems='center'
-							color='secondary'
-						>
-							<LocationOnOutlinedIcon
-								sx={{
-									color: iconColor,
-									fontSize: "16px",
-									marginRight: "6px",
-								}}
-							/>
-							<Typography
-								variant='caption'
-								component='span'
-							>
-								{task.locationType === "remote" ? "Remote" : task.locationName}
-							</Typography>
-						</Stack>
-
-						{/* Date */}
-						{task.dateType && (
+					<Stack
+						direction='row'
+						justifyContent='space-between'
+						alignItems='end'
+					>
+						<Box sx={{ paddingTop: "0.7rem" }}>
+							{/* Task Location */}
 							<Stack
 								direction='row'
 								alignItems='center'
+								color='secondary'
 							>
-								<CalendarMonthOutlinedIcon
+								<LocationOnOutlinedIcon
 									sx={{
 										color: iconColor,
 										fontSize: "16px",
@@ -91,38 +75,72 @@ const TaskItem = ({ task }) => {
 									variant='caption'
 									component='span'
 								>
-									{task.dateType === "flexible"
-										? "Flexible"
-										: task.dateType === "on"
-										? `on ${formatDate(task.date)}`
-										: `before ${formatDate(task.date)}`}
+									{task.locationType === "remote"
+										? "Remote"
+										: task.locationName}
 								</Typography>
 							</Stack>
-						)}
-					</Box>
-					<Divider sx={{ margin: "1rem 0" }} />
-					<Stack direction='row'>
-						{/* Task Budget */}
-						<Typography
-							variant='h6'
-							component='span'
-							sx={{ flexGrow: 1 }}
+							{/* Date */}
+							{task.dateType && (
+								<Stack
+									direction='row'
+									alignItems='center'
+								>
+									<CalendarMonthOutlinedIcon
+										sx={{
+											color: iconColor,
+											fontSize: "16px",
+											marginRight: "6px",
+										}}
+									/>
+									<Typography
+										variant='caption'
+										component='span'
+									>
+										{task.dateType === "flexible"
+											? "Flexible"
+											: task.dateType === "on"
+											? `on ${formatDate(task.date)}`
+											: `before ${formatDate(task.date)}`}
+									</Typography>
+								</Stack>
+							)}
+							{/* Posted BY */}
+							{posterName && (
+								<Stack
+									direction='row'
+									alignItems='center'
+								>
+									{/* TODO: Add profile icon */}
+									<PersonOutlineOutlinedIcon
+										sx={{
+											color: iconColor,
+											fontSize: "16px",
+											marginRight: "6px",
+										}}
+									/>
+									<Typography
+										variant='caption'
+										component='span'
+									>
+										{posterName}
+									</Typography>
+								</Stack>
+							)}
+						</Box>
+						<Stack
+							direction='column'
+							alignItems='flex-end'
 						>
-							{formatAmount(task.budget)}
-						</Typography>
-						{/* Posted Avatar */}
-						{task.postedBy && (
-							<Avatar
-								sx={{
-									backgroundColor: avatarColor,
-									width: "30px",
-									height: "30px",
-								}}
+							<Typography variant='caption'>Budget</Typography>
+							<Typography
+								variant='h6'
+								component='span'
+								sx={{ flexGrow: 1 }}
 							>
-								{posterName?.charAt(0)}
-							</Avatar>
-						)}
-						<div>{posterName}</div>
+								{formatAmount(task.budget)}
+							</Typography>
+						</Stack>
 					</Stack>
 				</CardContent>
 			</Card>
